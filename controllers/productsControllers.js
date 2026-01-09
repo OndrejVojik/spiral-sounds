f1d5e5f43eddf8b24d5de0343fbd651a97527f50
OndrejVojik <vojik.cz@gmail.com>
2026-01-09 19:46:44 +0100

    populate the DropDown

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,6 +1,21 @@
-export async function getGenres(){
+import { getDBConnection } from '../db/db.js'
 
-  console.log('genres')
+export async function getGenres(req, res) {
+
+  try {
+
+    const db = await getDBConnection()
+
+    const genreRows = await db.all('SELECT DISTINCT genre FROM products')
+    const genres = genreRows.map(row => row.genre)
+    console.log(genres)
+    res.json(genres)
+
+  } catch (err) {
+
+    res.status(500).json({error: 'Failed to fetch genres', details: err.message})
+
+  }
 
 }
 
