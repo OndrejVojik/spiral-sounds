8ebe683ab59ab1329cbc6a3e81ddbe5aef3e8f14
OndrejVojik <vojik.cz@gmail.com>
2026-01-13 19:01:15 +0100

    Adding to cart_table

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ import express from 'express'
 import { productsRouter } from './routes/products.js'
 import { authRouter } from './routes/auth.js'
 import { meRouter } from './routes/me.js'
+import { cartRouter } from './routes/cart.js' 
 import session from 'express-session'
 
 const app = express() 
 const PORT = 8000
 const secret = process.env.SPIRAL_SESSION_SECRET || 'jellyfish-baskingshark'
 
-app.use(express.json())
+app.use(express.json()) 
 
 app.use(session({
   secret: secret,
-  resave: false,
+  resave: false, 
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
@@ -29,6 +30,7 @@ app.use('/api/auth/me', meRouter)
 
 app.use('/api/auth', authRouter)
 
+app.use('/api/cart', cartRouter)
  
 app.listen(PORT, () => { 
   console.log(`Server running at http://localhost:${PORT}`)
--- sql/logTable.js
+++ sql/logTable.js
@@ -3,10 +3,9 @@ import { getDBConnection } from '../db/db.js'
 async function logTable() {
   const db = await getDBConnection()
 
-  const tableName = 'users'
+  const tableName = 'cart_items'
 
   try { 
-
     const table = await db.all(`SELECT * FROM ${tableName}`)
     console.table(table)
 
