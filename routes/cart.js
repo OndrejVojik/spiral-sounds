3b25adb210fbb1f37d062b8b0c71ce333f5bea04
OndrejVojik <vojik.cz@gmail.com>
2026-01-14 12:15:08 +0100

    Cart Page

--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,9 @@
 import express from 'express'
-import { addToCart, getCartCount } from '../controllers/cartController.js'
+import { addToCart, deleteAll, deleteItem, getAll, getCartCount } from '../controllers/cartController.js'
 
 export const cartRouter = express.Router()
 cartRouter.post('/add', addToCart) 
 cartRouter.get('/cart-count', getCartCount);
+cartRouter.get('/', getAll) 
+cartRouter.delete('/all', deleteAll) 
+cartRouter.delete('/:itemId', deleteItem)
